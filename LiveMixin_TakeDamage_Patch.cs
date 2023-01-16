144fb73bcbaa60b0c3574e792e97f3f78dac2e6e
Zemogiter <zumi999@vp.pl>
2023-01-16 18:29:24 +0100

    Update LiveMixin_TakeDamage_Patch.cs

--- LiveMixin_TakeDamage_Patch.cs
+++ LiveMixin_TakeDamage_Patch.cs
@@ -2,39 +2,40 @@ using System;
 using HarmonyLib;
 using UnityEngine;
 
-namespace DrillDamage;
-
-public class LiveMixin_TakeDamage_Patch
+namespace DrillDamage
 {
-    private static bool calling;
-
-    [HarmonyPrefix]
-    public static bool TakeDamage(LiveMixin __instance, DamageType type)
+    public class LiveMixin_TakeDamage_Patch
     {
-        try
+        private static bool calling;
+
+        [HarmonyPrefix]
+        public static bool TakeDamage(LiveMixin __instance, DamageType type)
         {
-            if (calling)
+            try
             {
-                return true;
+                if (calling)
+                {
+                    return true;
+                }
+                Plugin.Log("DrillDamage.LiveMixin.TakeDamage", 3);
+                if (type != DamageType.Drill || !Plugin.ConfigAffectCreatures.Value)
+                {
+                    return true;
+                }
+                Plugin.Log("DrillDamage.LiveMixin.TakeDamage Damage", 3);
+                if (__instance.health > 0f)
+                {
+                    calling = true;
+                    __instance.TakeDamage(Plugin.ConfigAdditionalDamage.Value, default, type);
+                    calling = false;
+                }
             }
-            Plugin.Log("DrillDamage.LiveMixin.TakeDamage", 3);
-            if (type != DamageType.Drill || !(bool)Plugin.ConfigAffectCreatures.BoxedValue)
+            catch (Exception ex)
             {
-                return true;
+                Plugin.Log(ex.Message + "\r\n" + ex.StackTrace, 0);
+                throw;
             }
-            Plugin.Log("DrillDamage.LiveMixin.TakeDamage Damage", 3);
-            if (__instance.health > 0f)
-            {
-                calling = true;
-                __instance.TakeDamage((int)Plugin.ConfigAdditionalDamage.BoxedValue, default, type);
-                calling = false;
-            }
+            return true;
         }
-        catch (Exception ex)
-        {
-            Plugin.Log(ex.Message + "\r\n" + ex.StackTrace, 0);
-            throw;
-        }
-        return true;
     }
 }
