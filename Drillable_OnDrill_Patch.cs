7b8dacb56e12a3d5421692c6039f4a8fb478624a
Zemogiter <zumi999@vp.pl>
2023-01-17 17:27:18 +0100

    Add files via upload
    Using transpillers for exosuit drill and a separate patch for seamoth arms mod

--- Drillable_OnDrill_Patch.cs
+++ Drillable_OnDrill_Patch.cs
@@ -1,102 +1,64 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Security.AccessControl;
 using BepInEx.Configuration;
+using DrillDamage;
 using HarmonyLib;
-using UnityEngine;
-using static UnityEngine.UI.DefaultControls;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System;
 
-namespace DrillDamage
+[HarmonyPatch(typeof(Drillable), nameof(Drillable.OnDrill))]
+internal class Drillable_Transpiler_Patch
 {
-    public class Drillable_OnDrill_Patch
+    [HarmonyTranspiler]
+    internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> cins)
     {
-        private MeshRenderer[] renderers;
-        public Drillable.ResourceType[] resources;
-        [HarmonyPrefix]
-        public bool OnDrill(Drillable __instance, Vector3 position)
-        {
-            try
-            {
-                Plugin.Log("DrillDamage.OnDrill " + __instance.GetType().FullName, 3);
-                Traverse traverse = null;
-                string fullName = __instance.GetType().FullName;
-                traverse = (fullName == null || !(fullName == "ModdedArmsHelper.API.SeamothDrillable")) ? Traverse.Create(__instance).Field("health") : Traverse.Create(__instance).Field("drillable").Field("health");
-                float num = 0f;
-                for (int i = 0; i < traverse.GetValue<float[]>().Length; i++)
-                {
-                    num += traverse.GetValue<float[]>()[i];
-                }
-                if (num > 0f)
-                {
-                    int num2 = FindClosestMesh(position, out Vector3 center);
-                    traverse.GetValue<float[]>()[num2] = Mathf.Max(1f, traverse.GetValue<float[]>()[num2] - Plugin.ConfigAdditionalDamage.Value);
-                }
-                if (Plugin.ConfigVariableModeEnabled.Value == true)
-                {
-                    Dictionary<TechType, int> ConfigDictionary = new()
-                    {
-                        { TechType.DrillableSalt, Plugin.ConfigDrillableSaltDamage.Value },
-                        { TechType.DrillableQuartz, Plugin.ConfigDrillableQuartzDamage.Value },
-                        { TechType.DrillableCopper, Plugin.ConfigDrillableCopperDamage.Value },
-                        { TechType.DrillableTitanium, Plugin.ConfigDrillableTitaniumDamage.Value },
-                        { TechType.DrillableLead, Plugin.ConfigDrillableLeadDamage.Value },
-                        { TechType.DrillableSilver, Plugin.ConfigDrillableSilverDamage.Value },
-                        { TechType.DrillableDiamond, Plugin.ConfigDrillableDiamondDamage.Value },
-                        { TechType.DrillableGold, Plugin.ConfigDrillableGoldDamage.Value },
-                        { TechType.DrillableMagnetite, Plugin.ConfigDrillableMagnetiteDamage.Value },
-                        { TechType.DrillableLithium, Plugin.ConfigDrillableLithiumDamage.Value },
-                        { TechType.DrillableMercury, Plugin.ConfigDrillableMercuryDamage.Value },
-                        { TechType.DrillableUranium, Plugin.ConfigDrillableUraniumDamage.Value },
-                        { TechType.DrillableAluminiumOxide, Plugin.ConfigDrillableAluminiumOxideDamage.Value },
-                        { TechType.DrillableNickel, Plugin.ConfigDrillableNickelDamage.Value },
-                        { TechType.DrillableSulphur, Plugin.ConfigDrillableSulphurDamage.Value },
-                        { TechType.DrillableKyanite, Plugin.ConfigDrillableKyaniteDamage.Value }
-                    };
-                    int num2 = FindClosestMesh(position, out Vector3 center);
-                    Plugin.Log("Result of FindClosestMesh = " + num2, 2);
-                    TechType key = __instance.GetDominantResourceType();
-                    Plugin.Log("The techType is = " + key, 2);
-                    var valueGet = ConfigDictionary.TryGetValue(key, out int value);
-                    Plugin.Log("Value is = " + value, 2);
-                    traverse.GetValue<float[]>()[num2] = Mathf.Max(1f, traverse.GetValue<float[]>()[num2] - value);
+        var matcher = new CodeMatcher(cins);
 
-                }
-            }
-            catch (Exception ex)
-            {
-                Plugin.Log(ex.Message + "\r\n" + ex.StackTrace, 0);
-                throw;
-            }
-            return true;
-        }
-        private int FindClosestMesh(Vector3 position, out Vector3 center)
+        matcher.MatchForward(false, new CodeMatch(OpCodes.Ldc_R4, 5f));
+        if (matcher.IsValid)
         {
-            int result = 0;
-            float num = float.PositiveInfinity;
-            center = Vector3.zero;
-
-            for (int i = 0; i < renderers.Length; i++)
-            {
-                if (renderers[i].gameObject.activeInHierarchy)
-                {
-                    Bounds encapsulatedAABB = UWE.Utils.GetEncapsulatedAABB(renderers[i].gameObject, -1);
-                    float sqrMagnitude = (encapsulatedAABB.center - position).sqrMagnitude;
-                    if (sqrMagnitude < num)
-                    {
-                        num = sqrMagnitude;
-                        result = i;
-                        center = encapsulatedAABB.center;
-                        if (sqrMagnitude <= 0.5f)
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-            return result;
+            matcher.SetAndAdvance(OpCodes.Ldarg_0, null);
+            matcher.Insert(new CodeInstruction(OpCodes.Call, typeof(Drillable_Transpiler_Patch).GetMethod(nameof(GetDamageForDrillable))));
+            return matcher.InstructionEnumeration();
         }
+
+        Console.WriteLine($"Failed to find matching instructions for {nameof(Drillable_Transpiler_Patch)}. Either another mod has already transpiled this or the games code has changed.");
+        return cins;
+    }
+
+    private static Dictionary<TechType, ConfigEntry<int>> ConfigDictionary = new(){
+        { TechType.DrillableSalt, Plugin.ConfigDrillableSaltDamage },
+        { TechType.DrillableQuartz, Plugin.ConfigDrillableQuartzDamage },
+        { TechType.DrillableCopper, Plugin.ConfigDrillableCopperDamage },
+        { TechType.DrillableTitanium, Plugin.ConfigDrillableTitaniumDamage },
+        { TechType.DrillableLead, Plugin.ConfigDrillableLeadDamage },
+        { TechType.DrillableSilver, Plugin.ConfigDrillableSilverDamage },
+        { TechType.DrillableDiamond, Plugin.ConfigDrillableDiamondDamage },
+        { TechType.DrillableGold, Plugin.ConfigDrillableGoldDamage },
+        { TechType.DrillableMagnetite, Plugin.ConfigDrillableMagnetiteDamage },
+        { TechType.DrillableLithium, Plugin.ConfigDrillableLithiumDamage },
+        { TechType.DrillableMercury, Plugin.ConfigDrillableMercuryDamage },
+        { TechType.DrillableUranium, Plugin.ConfigDrillableUraniumDamage },
+        { TechType.DrillableAluminiumOxide, Plugin.ConfigDrillableAluminiumOxideDamage },
+        { TechType.DrillableNickel, Plugin.ConfigDrillableNickelDamage },
+        { TechType.DrillableSulphur, Plugin.ConfigDrillableSulphurDamage },
+        { TechType.DrillableKyanite, Plugin.ConfigDrillableKyaniteDamage }
+    };
+
+    private static float GetDamageForDrillable(Drillable drillable)
+    {
+        float newDamage;
+        if (Plugin.ConfigVariableModeEnabled.Value == false)
+        {
+            newDamage = Plugin.ConfigAdditionalDamage.Value;
+        }
+        else
+        {
+            TechType key = drillable.GetDominantResourceType();
+            Plugin.Log("The techType is = " + key, 2);
+            var valueGet = ConfigDictionary.TryGetValue(key, out var config);
+            Plugin.Log("Value is = " + config.Value, 2);
+            newDamage = config.Value;
+        }
+        return newDamage;
     }
 }
---
+++ SeamothDrillable_Patch.cs
@@ -0,0 +1,53 @@
+﻿using DrillDamage;
+using HarmonyLib;
+using static VFXParticlesPool;
+using System.Collections.Generic;
+using System.Reflection;
+
+[HarmonyPatch(typeof(Drillable))]
+[HarmonyPatch("Start")]
+internal class SeamothDrillable_Patch
+{
+    [HarmonyPostfix]
+    public void Patch(Drillable __instance)
+    {
+        var seamothDrillable = __instance.gameObject.GetComponent("SeamothDrillable");
+
+        if (seamothDrillable != null) // if SeamothDrillable component is available on resource, processing the drill damage value else do nothing
+        {
+            PropertyInfo drillDamage = seamothDrillable.GetType().GetProperty("drillDamage");
+
+            if (Plugin.ConfigAffectSeamothArms.Value == true && Plugin.ConfigVariableModeEnabled.Value == false)
+            {
+                drillDamage.SetValue(seamothDrillable, Plugin.ConfigAdditionalDamage);
+            }
+            else if (Plugin.ConfigAffectSeamothArms.Value == true && Plugin.ConfigVariableModeEnabled.Value == true)
+            {
+                Dictionary<TechType, int> ConfigDictionary = new()
+                {
+                    { TechType.DrillableSalt, Plugin.ConfigDrillableSaltDamage.Value },
+                    { TechType.DrillableQuartz, Plugin.ConfigDrillableQuartzDamage.Value },
+                    { TechType.DrillableCopper, Plugin.ConfigDrillableCopperDamage.Value },
+                    { TechType.DrillableTitanium, Plugin.ConfigDrillableTitaniumDamage.Value },
+                    { TechType.DrillableLead, Plugin.ConfigDrillableLeadDamage.Value },
+                    { TechType.DrillableSilver, Plugin.ConfigDrillableSilverDamage.Value },
+                    { TechType.DrillableDiamond, Plugin.ConfigDrillableDiamondDamage.Value },
+                    { TechType.DrillableGold, Plugin.ConfigDrillableGoldDamage.Value },
+                    { TechType.DrillableMagnetite, Plugin.ConfigDrillableMagnetiteDamage.Value },
+                    { TechType.DrillableLithium, Plugin.ConfigDrillableLithiumDamage.Value },
+                    { TechType.DrillableMercury, Plugin.ConfigDrillableMercuryDamage.Value },
+                    { TechType.DrillableUranium, Plugin.ConfigDrillableUraniumDamage.Value },
+                    { TechType.DrillableAluminiumOxide, Plugin.ConfigDrillableAluminiumOxideDamage.Value },
+                    { TechType.DrillableNickel, Plugin.ConfigDrillableNickelDamage.Value },
+                    { TechType.DrillableSulphur, Plugin.ConfigDrillableSulphurDamage.Value },
+                    { TechType.DrillableKyanite, Plugin.ConfigDrillableKyaniteDamage.Value }
+                };
+                TechType key = __instance.GetDominantResourceType();
+                Plugin.Log("The techType is = " + key, 2);
+                var valueGet = ConfigDictionary.TryGetValue(key, out int value);
+                Plugin.Log("Value is = " + value, 2);
+                drillDamage.SetValue(seamothDrillable, value);
+            }
+        }
+    }
+}
